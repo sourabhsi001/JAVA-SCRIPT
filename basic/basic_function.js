dd3d3392ba939ed96ed4407e666fac6cefbb5cfb
sourabhsi001 <singhsourabh38586@gmail.com>
2026-06-24 11:11:15 +0530

    day 1 basic

---
+++ basic/basic_function.js
@@ -0,0 +1,28 @@
+let arr=[1,2,3,4,5]
+// // Removes element from the end
+// arr.pop()
+// console.log(arr);
+
+// // Adds element at the end
+// arr.push(6);
+// console.log(arr);
+
+// // Adds element at the beginning
+// arr.unshift(1);
+// console.log(arr);
+
+// // Removes element from the beginning
+// arr.unshift(1);
+// console.log(arr)
+
+
+
+// splice() array ke andar add, remove, ya replace karne ke liye use hota hai.
+
+// arr.splice(1,2);//index 1 se start kerna he aur 2 element delete kerna he 
+// console.log(arr);
+
+// arr.splice(1,0,0,0);//delete count 0 he so use ke baad jo bh i number he vo add kerna he index ke baad
+// console.log(arr);
+
+//
---
+++ basic/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+Adds element at the end
