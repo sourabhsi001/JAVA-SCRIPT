1ab29ef9c9ae39d69d19933eae8a4aca24620235
sourabhsi001 <singhsourabh38586@gmail.com>
2026-06-28 22:53:00 +0530

    function add

---
+++ functiion.js
@@ -0,0 +1,45 @@
+// function name(arguments)
+// {
+//    javascript statements
+// }
+// basic syntex of function
+
+// function welcome() {
+//     console.log("welcome to GfG");
+// }
+
+// // Function calling
+// welcome();
+
+
+// function welcome(name) {
+//     console.log("Hey " + "" + name + " " + "welcome to GfG");
+// }
+
+// // Passing arguments
+// welcome("Rohan");
+
+
+// function welcome() {
+
+//     // Return statement
+//     return "Welcome to GfG";
+// }
+
+// console.log(welcome());
+
+
+////function expression
+// let welcome = function () {
+//     return "Welcome to GfG";
+// }
+
+// let gfg = welcome();
+
+// console.log(gfg);
+
+
+
+
+
+
